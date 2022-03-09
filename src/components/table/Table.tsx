2c21bfad4a90b79297c414420709100fd3bcebc4
Xavi <84887643+Xavi-vmo@users.noreply.github.com>
2022-03-09 10:01:24 +0700

    create list type user page (#3)

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,79 +1,135 @@
+/* eslint-disable import/order */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-undef */
+/* eslint-disable react/no-unused-prop-types */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, {useRef, useState} from 'react';
 import useOnClickOutside from '@app/hooks/useClickOutside';
 import threeDotIcon from '../../static/icon/threedot.svg';
 import MoreAction from '../moreAction/MoreAction';
 import '../../styles/components/table.scss';
+import {Table} from 'antd';
+import {useRef, useState} from 'react';
+import EmptyImg from '../../static/img/404.png';
 
-const dataTable = [
-  {
-    id: 0,
-    name: 'Admin Vận Hành',
-    creator: 'LongTT',
-    createdDate: '01/01/2022'
-  },
-  {
-    id: 1,
-    name: 'Admin 1',
-    creator: 'LongTT',
-    createdDate: '01/01/2022'
-  }
-];
+interface DataType {
+  key: React.Key;
+  name: string;
+  creator: string;
+  createdDate: string;
+}
 
-function Table() {
+// const data: DataType[] = [
+//   {
+//     key: '1',
+//     name: 'John Brown',
+//     creator: 'LongTT',
+//     createdDate: '01/01/2022'
+//   },
+//   {
+//     key: '2',
+//     name: 'Jim Green',
+//     creator: 'LongTT',
+//     createdDate: '01/01/2022'
+//   },
+//   {
+//     key: '3',
+//     name: 'Joe Black',
+//     creator: 'LongTT',
+//     createdDate: '01/01/2022'
+//   },
+//   {
+//     key: '4',
+//     name: 'Disabled User',
+//     creator: 'LongTT',
+//     createdDate: '01/01/2022'
+//   }
+// ];
+
+const data: DataType[] = [];
+const TableCustom = () => {
+  // const {data = []} = props;
   const ref = useRef(null);
   useOnClickOutside(ref, () => setIsShowModal(-1));
   const [isShowModal, setIsShowModal] = useState<number>(-1);
   const handleShowModal = (index: number) => {
     setIsShowModal(index);
   };
-  return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <input
-              className="form-check-input big-checkbox"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-            />
-          </th>
-          <th>Kiểu người dùng</th>
-          <th>Người tạo</th>
-          <th>Ngày tạo</th>
-          <th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
-        </tr>
-      </thead>
-      <tbody>
-        {dataTable.map((user, index) => (
-          <tr>
-            <td>
-              <div className="form-check">
-                <input
-                  className="form-check-input big-checkbox"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-              </div>
-            </td>
-            <td>{user.name}</td>
-            <td>{user.creator}</td>
-            <td>{user.createdDate}</td>
-            <td>
-              <div onClick={() => handleShowModal(index)} className="btn">
-                <img src={threeDotIcon} alt="icon" />
-                <div ref={ref}>{isShowModal === index && <MoreAction />}</div>
-              </div>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
 
-export default Table;
+  const [selectionType, setSelectionType] = useState<'checkbox' | 'radio'>(
+    'checkbox'
+  );
+  const columns = [
+    {
+      title: 'Kiểu người dùng',
+      dataIndex: 'name',
+      render: (text: string) => <a>{text}</a>
+    },
+    {
+      title: 'Người tạo',
+      dataIndex: 'creator'
+    },
+    {
+      title: 'Ngày tạo',
+      dataIndex: 'createdDate'
+    },
+    {
+      title: '',
+      dataIndex: 'key',
+      render: (key: any) => (
+        <div onClick={() => handleShowModal(key)} className="btn">
+          <img src={threeDotIcon} alt="icon" />
+          <div ref={ref}>{isShowModal === key && <MoreAction />}</div>
+        </div>
+      )
+    }
+  ];
+
+  const rowSelection = {
+    onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
+      console.log(
+        `selectedRowKeys: ${selectedRowKeys}`,
+        'selectedRows: ',
+        selectedRows
+      );
+    },
+    getCheckboxProps: (record: DataType) => ({
+      disabled: record.name === 'Disabled User', // Column configuration not to be checked
+      name: record.name
+    })
+  };
+  return (
+    <>
+      {data.length > 0 ? (
+        <>
+          <Table
+            rowSelection={{
+              type: selectionType,
+              ...rowSelection
+            }}
+            columns={columns}
+            dataSource={data}
+          />
+        </>
+      ) : (
+        <>
+          <div className="box-overview">
+            <p>
+              Tổng: <span>{data.length} kiểu người dùng</span>
+            </p>
+          </div>
+          <div className="container-nodata">
+            <div className="box-nodata">
+              <img src={EmptyImg} alt="empty-user" />
+              <p>Chưa có kiểu người dùng nào trong hệ thống</p>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default TableCustom;
