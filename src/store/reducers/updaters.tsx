37eb4ab3571d418b52915e82cee25407bb69a049
sinhnt <sinhnt@vmodev.com>
2022-03-31 13:38:11 +0700

    coding api player manager

--- src/store/reducers/updaters.tsx
+++ src/store/reducers/updaters.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable no-debugger */
-// import {getRoles} from "@app/utils";
+import { getRoles } from "@app/utils";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-// import {setUserRoles} from "./user";
+import { setUserRoles } from "./user";
 
 export default function Updater(): null {
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getAllData = async () => {
-      // const dataRoles = await getRoles();
-      // dispatch(setUserRoles(dataRoles));
+      const dataRoles = await getRoles();
+      dispatch(setUserRoles(dataRoles));
     };
     getAllData();
   }, [dispatch]);
